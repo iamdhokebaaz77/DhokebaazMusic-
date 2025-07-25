b69f471cc5b42b3484ce4c1d6abbd1006793ca62
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-07-25 14:13:20 +0530

    Update start.py

--- IstkharMusic/utils/inline/start.py
+++ IstkharMusic/utils/inline/start.py
@@ -27,10 +27,11 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["https://t.me/dhokebaazgc"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["https://t.me/SexyNayak"], url=config.SUPPORT_CHANNEL),
-             ],
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="sσυʀᴄє ᴄσᴅє", url=f"https://github.com/TEAM-ISTKHAR/IstkharMusic.git"),
+        ],
     ]
     return buttons
